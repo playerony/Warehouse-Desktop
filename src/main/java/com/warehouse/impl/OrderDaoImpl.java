d9f282734e94256398c3024a0000b5b93bbb12cd
playerony <playerony@gazeta.pl>
2017-02-15 17:34:16 +0100

    Create OrderDaoImpl class

---
+++ src/main/java/com/warehouse/impl/OrderDaoImpl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.impl;
+
+import com.warehouse.dao.OrderDao;
+import com.warehouse.entity.Order;
+import com.warehouse.service.OrderService;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class OrderDaoImpl implements OrderDao{
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    @Override
+    public boolean checkOrderById(int id) {
+        for(Order o : OrderService.list(sessionFactory))
+            if(o.getId() == id)
+                return true;
+        
+        return false;
+    }
+    
+    @Override
+    public Order getOrderById(int id){
+        for(Order o : OrderService.list(sessionFactory))
+            if(o.getId() == id)
+                return o;
+        
+        return null;
+    }
+    
+    @Override
+    public int getClientID(int id){
+        for(Order o : OrderService.list(sessionFactory))
+            if(o.getId() == id)
+                return o.getClient().getId();
+        
+        return -1;
+    }
+}
