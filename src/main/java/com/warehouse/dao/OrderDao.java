3397f48c139a81dd44f50db22bd364e07160b187
playerony <playerony@gazeta.pl>
2017-02-15 17:34:05 +0100

    Create OrderDao class

---
+++ src/main/java/com/warehouse/dao/OrderDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.dao;
+
+import com.warehouse.entity.Order;
+import org.hibernate.exception.GenericJDBCException;
+
+/**
+ *
+ * @author pawel_000
+ */
+public interface OrderDao {
+    public boolean checkOrderById(int id) throws GenericJDBCException;
+  
+    public Order getOrderById(int id);
+    
+    public int getClientID(int id);
+}
