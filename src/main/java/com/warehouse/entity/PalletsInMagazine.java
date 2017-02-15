d0c9ec9253bdc9a32c3ac1a70463bba944dedb4b
playerony <playerony@gazeta.pl>
2017-02-15 19:03:46 +0100

    Create PalletsInMagazine entity class

---
+++ src/main/java/com/warehouse/entity/PalletsInMagazine.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pawel_000
+ */
+
+@Entity
+@Table(name="pallets_in_magazine")
+public class PalletsInMagazine implements Serializable {
+    
+    private int id;
+    private String products;
+    private String location;
+    
+    public PalletsInMagazine(){
+        
+    }
+    
+    public PalletsInMagazine(int id, String location){
+        this.id = id;
+        this.location = location;
+    }
+    
+    public PalletsInMagazine(int id, String products, String location){
+        this.id = id;
+        this.products = products;
+        this.location = location;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
