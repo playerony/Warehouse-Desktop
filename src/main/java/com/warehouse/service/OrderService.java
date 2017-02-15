4cb70fcf1651939f6135ff40c6b77953cd73b155
playerony <playerony@gazeta.pl>
2017-02-15 17:10:24 +0100

    Create OrderService class

---
+++ src/main/java/com/warehouse/service/OrderService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.service;
+
+import com.warehouse.entity.Order;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.exception.GenericJDBCException;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class OrderService {
+    public static List<Order> list(SessionFactory sessionFactory)  throws GenericJDBCException{
+        Session session = sessionFactory.openSession();
+        List<Order> list = session.createQuery("from Order").list();
+        session.close();
+        
+        return list;
+    }
+}
