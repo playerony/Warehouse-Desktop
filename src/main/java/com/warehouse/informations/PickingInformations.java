fdd1f06f1e3798dfeb194d9fefdc0aa6c104940c
playerony <playerony@gazeta.pl>
2017-02-15 20:10:38 +0100

    Create PickingInformation class

---
+++ src/main/java/com/warehouse/informations/PickingInformations.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.informations;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class PickingInformations extends AbstractInformation{
+    private String workerLogin;
+    private String itemCode;
+
+    public String getWorkerLogin() {
+        return workerLogin;
+    }
+
+    public void setWorkerLogin(String workerLogin) {
+        this.workerLogin = workerLogin;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+}
