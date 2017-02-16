e3cb26a71ac89bfda53cd16688ec651d620153ce
playerony <playerony@gazeta.pl>
2017-02-16 15:51:36 +0100

    Create simple *.pdf generator

--- src/main/java/com/warehouse/abstractController/OrderMenuAbstractController.java
+++ src/main/java/com/warehouse/abstractController/OrderMenuAbstractController.java
@@ -12,16 +12,28 @@ import java.io.FileOutputStream;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.warehouse.cookie.Cookie;
+import com.warehouse.entity.Order;
 import com.warehouse.impl.OrderDaoImpl;
+import com.warehouse.loader.LoadFXML;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
  * @author pawel_000
  */
 public class OrderMenuAbstractController{
-    protected OrderDao orderDao = new OrderDaoImpl();
+    protected OrderDao orderDao;
+    
+    public void init() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        orderDao = context.getBean(OrderDaoImpl.class);
+    }
 
     public void handleGeneratePDF() {
         try {
@@ -30,16 +42,40 @@ public class OrderMenuAbstractController{
             PdfWriter.getInstance(document, file);
             document.open();
             
-            document.add(new Paragraph("PDF test"));
+            document.add(new Paragraph("Order information"));
+            
+            for(Order o : orderDao.getOrderList())
+                if(o.getId() == Integer.parseInt(Cookie.getInstance().get("orderID")))
+                    document.add(new Paragraph("Order (" + o.getId() + ") " + o.getItems() + " " + o.getDate() + " " + o.getClient().getPhone()));
 
             document.add(new Paragraph(new Date().toString()));
             document.close();
 
             file.close();
 
-            AlertBox.getInstance().display(getClass().getSimpleName().toString(), "Successful generated *.PDF file !");
+            AlertBox.getInstance().display(getClass().getSimpleName(), "Successful generated *.PDF file !");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    
+    public void handleAddUserAction() {
+        try{
+            LoadFXML.getInstance().loadFile("addUserPanel");
+        }catch(IOException e){
+            AlertBox.getInstance().display(getClass().getSimpleName(), "Some problems by loading addUserPanel.fxml");
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+    
+    public void handleDeleteUserAction() {
+        try{
+            LoadFXML.getInstance().loadFile("deleteUserPanel");
+        }catch(IOException e){
+            AlertBox.getInstance().display(getClass().getSimpleName(), "Some problems by loading deleteUserPanel.fxml");
+            e.printStackTrace();
+            System.exit(0);
         }
     }
 
