3dfcab9b4be0eafa73ecec6f00594f7a5a48a565
playerony <playerony@gazeta.pl>
2017-02-16 11:24:10 +0100

    Add Delete and Insert method in UserService class

--- src/main/java/com/warehouse/service/UserService.java
+++ src/main/java/com/warehouse/service/UserService.java
@@ -6,9 +6,11 @@
 package com.warehouse.service;
 
 import com.warehouse.entity.User;
+import java.io.IOException;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.exception.GenericJDBCException;
 
 /**
@@ -16,6 +18,24 @@ import org.hibernate.exception.GenericJDBCException;
  * @author pawel_000
  */
 public class UserService {
+    public static void insert(User user, SessionFactory sessionFactory) throws IOException{
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(user);
+        
+        transaction.commit();
+        session.close();
+    }
+    
+    public static void delete(User user, SessionFactory sessionFactory) throws IOException{
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        
+        transaction.commit();
+        session.close();
+    }
+    
     public static List<User> list(SessionFactory sessionFactory)  throws GenericJDBCException{
         Session session = sessionFactory.openSession();
         List<User> list = session.createQuery("from User").list();
